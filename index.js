706217d96d8ea7da54aaee79cbabbefbeebc2806
SamChongLuke <samanthachongluke@yahoo.ca>
2021-02-24 23:50:07 -0500

    updated to link to homepage

---
+++ index.js
@@ -0,0 +1,63 @@
+var helloWorld = "helloWorld";
+console.log(helloWorld);
+
+$("#nav").hover(function(){
+    $(this).css("text-decoration", "underline");
+  });
+
+  $("#nav").mouseout(function(){
+      $("#nav").css("text-decoration", "none");
+  });
+
+  $("#nav2").hover(function(){
+    $(this).css("text-decoration", "underline");
+  });
+
+  $("#nav2").mouseout(function(){
+      $("#nav2").css("text-decoration", "none");
+  });
+
+  $("#nav3").hover(function(){
+    $(this).css("text-decoration", "underline");
+  });
+
+  $("#nav3").mouseout(function(){
+      $("#nav3").css("text-decoration", "none");
+  });
+
+$("#resumeLink").mouseover(function(){
+    $("#resumeLink").css("background-color", "#AEDECF");
+    $("#resumeLink").css("border-color", "#AEDECF");
+    $("#resumeLink").css("font-weight", "500");
+  });
+
+  $("#resumeLink").mouseout(function(){
+    $("#resumeLink").css("background-color", "#FDF6F0");
+    $("#resumeLink").css("border-color", "#333333");
+    $("#resumeLink").css("font-weight", "lighter");
+
+  });
+
+  $("#resumeLink").on("click", function(){
+    $("#resumeLink").css("font-weight","bold");
+    $("#resumeLink").css("background-color","#AEDECF");
+    $("#resumeLink").css("border-color","#AEDECF");
+    window.open(
+        'https://drive.google.com/file/d/1unvXotA_DApSFkhMVXhH94PwaVYTyVmp/view?usp=sharing',
+        '_blank'
+      );
+  });
+
+$(".logo").on("click", function(){
+    window.location.replace('https://samchongluke.github.io/UX_UI_HW_19/');
+
+});
+
+
+$("#linkedin").on("click", function(){
+    window.open('https://www.linkedin.com/in/samantha-chong-luke-363796102/');
+});
+
+$("#github").on("click", function(){
+    window.open('https://github.com/SamChongLuke');
+});
